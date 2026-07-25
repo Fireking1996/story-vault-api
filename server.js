243972344c5577241922288bcb0a57918ca34e4a
Fireking1996 <firebringer1996@yahoo.com>
2026-07-25 10:29:22 -0500

    update

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const ideasRoute = require("./routes/ideas");
 const authRoute = require("./routes/auth");
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(cors());
 
 app.use(express.json());
